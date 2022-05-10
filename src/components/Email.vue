f0168ac5fb2c3b00ba902fa2d27a8c127621a056
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-05-10 11:41:56 -0500

    Fixed some styles on social and email component

--- src/components/Email.vue
+++ src/components/Email.vue
@@ -2,12 +2,12 @@
     <div name="email" className="hidden md:flex fixed flex-col bottom-[410px] w-full">
         <ul className="flex fixed right-2">
             <li>
-                <a className='flex justify-between items-center text-[#f9b82c] dark:text-white' href='mailto:hernandez.mario@correounivalle.edu.co' target='_blank' rel="noreferrer"> 
+                <a className='flex justify-between items-center text-[#f9b82c] dark:text-white animate__animated animate__slideInRight' href='mailto:hernandez.mario@correounivalle.edu.co' target='_blank' rel="noreferrer"> 
                    <p :style="{'writingMode': 'vertical-rl'}" className=" text-sm hover:text-pink-600 duration-300">hernandez.mario@correounivalle.edu.co</p>
                 </a>
             </li>
         </ul>
-        <div className="w-[2px] h-[140px] flex fixed bottom-[0px] border border-[#0a192f] dark:border-[#f9b82c] right-[1.15rem] mx-auto mt-2"/>
+        <div className="w-[2px] h-[140px] flex fixed bottom-[0px] border border-[#0a192f] dark:border-[#f9b82c] right-[1.15rem] mx-auto mt-2 animate__animated animate__backInUp"/>
     </div>
 </template>
 
--- src/components/Social.vue
+++ src/components/Social.vue
@@ -2,28 +2,28 @@
     <!-- Close Button -->
     <div className='hidden md:flex fixed flex-col bottom-[150px] left-[0.5rem]'>
         <ul className="animate__bounce">
-            <li className='w-[100px] h-[50px] flex justify-between items-center'>
+            <li className='w-[100px] h-[50px] flex justify-between items-center animate__animated animate__slideInLeft'>
                 <a className='flex justify-between items-center w-full text-[#f9b82c] dark:text-white duration-300' href='https://www.linkedin.com/in/mario1927/' target='_blank' rel="noreferrer"> 
                    <fa :icon="['fab', 'linkedin']" class="fa-xl" />
                 </a>
             </li>
-            <li className='w-[100px] h-[50px] flex justify-between items-center'>
+            <li className='w-[100px] h-[50px] flex justify-between items-center animate__animated animate__slideInLeft'>
                 <a className='flex justify-between items-center w-full text-[#f9b82c] dark:text-white duration-300' href='https://github.com/Mario1927' target='_blank' rel="noreferrer"> 
                     <fa :icon="['fab', 'github']" class="fa-xl" />
                 </a>
             </li>
-            <li className='w-[100px] h-[50px] flex justify-between items-center'>
+            <li className='w-[100px] h-[50px] flex justify-between items-center animate__animated animate__slideInLeft'>
                 <a className='flex justify-between items-center w-full text-[#f9b82c] dark:text-white duration-300' href='mailto:hernandez.mario@correounivalle.edu.co' target='_blank' rel="noreferrer"> 
                    <fa icon="envelope" class="fa-xl" />
                 </a>
             </li>
-            <li className='w-[100px] h-[50px] flex justify-between items-center'>
+            <li className='w-[100px] h-[50px] flex justify-between items-center animate__animated animate__slideInLeft'>
                 <a className='flex justify-between items-center w-full text-[#f9b82c] dark:text-white duration-300' :href="link" target='_blank' rel="noreferrer"> 
                     <span class="mx-1"><fa icon="file-pdf" class="fa-xl" /></span>
                 </a>
             </li>
         </ul>
-        <div className="w-[2px] h-[140px] flex fixed bottom-[0px] left-[1.15rem] border border-[#0a192f] dark:border-[#f9b82c] mx-auto mt-2"/>
+        <div className="w-[2px] h-[140px] flex fixed bottom-[0px] left-[1.15rem] border border-[#0a192f] dark:border-[#f9b82c] mx-auto mt-2 animate__animated animate__backInUp"/>
     </div>
 </template>
 
